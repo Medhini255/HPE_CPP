f49b8d1dbb9372cb99037e3e97234a9ace8e8927
Medhini255 <medhini2004m@gmail.com>
2025-07-15 15:47:22 +0530

    Add files via upload

--- Docker/Benchmark/Victoria-Metrics/benchmark_query.py
+++ Docker/Benchmark/Victoria-Metrics/benchmark_query.py
@@ -1,1 +1,12 @@
+import requests
+import time
 
+durations = []
+for i in range(100):
+    start = time.time()
+    r = requests.get("http://localhost:8428/api/v1/query", params={
+        "query": "rate(benchmark_metric[1m])"
+    })
+    durations.append(time.time() - start)
+
+print(f"Average query latency: {sum(durations)/len(durations):.4f} sec") 
